18e990ab5acf81c9cc14a77bb7abf1277b2207f6
Rich Barlow <rsb177@hotmail.com>
2020-10-22 16:08:14 -0400

    Created CLI option for zip code.

--- my_weather/main.py
+++ my_weather/main.py
@@ -4,9 +4,21 @@ import typer
 app = typer.Typer()
 
 
-@app.command()
-def main():
+def get_current_weather(zip_code: str) -> None:
     pass
+
+
+@app.command()
+def main(
+    zip_code: str = typer.Option(
+        ...,
+        "--zip",
+        "-z",
+        prompt=True,
+        help="Zip code to get the weather for.",
+    ),
+):
+    get_current_weather(zip_code=zip_code)
 
 
 if __name__ == "__main__":
