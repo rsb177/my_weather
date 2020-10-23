de036d4c721d80f2fb96c15c9e705b9868cce8f4
Rich Barlow <rsb177@hotmail.com>
2020-10-23 15:23:30 -0400

    Added in API call to get current weather.

--- my_weather/main.py
+++ my_weather/main.py
@@ -1,11 +1,20 @@
+import json
+import os
+
+import requests
 import typer
 
-
 app = typer.Typer()
+OWM_BASE_URL: str = "https://api.openweathermap.org/data/2.5/"
+OWM_API_KEY: str = os.environ["OWM_API_KEY"]
 
 
 def get_current_weather(zip_code: str) -> None:
-    pass
+    response = requests.get(
+        f"{OWM_BASE_URL}weather?zip={zip_code},us&appid={OWM_API_KEY}"
+    )
+    # for j in response.json():
+    typer.echo(json.dumps(response.json(), indent=2))
 
 
 @app.command()
